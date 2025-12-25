68f8d0e38a41ba8b4e766ee8bbae44cf1dc3b382
liliacls <chals.lilia@gmail.com>
2025-12-25 22:24:08 +0100

    Update comments

--- src/stats_utils.py
+++ src/stats_utils.py
@@ -1,20 +1,26 @@
-### En entrée : 
-### --> dico {gène : liste des comptages par tissus/cellules}
-### --> liste des tissus/cellules (même ordre que les comptages)
-### En sortie : 
-### --> dico {gène : somme des comptages sur tous les tissus/cellules}
-### --> listes des gènes/tissus associés aux valeurs min et max 
-
 def total_count_gene(data):
 
-### Fonction qui calcul le nombre total de comptages pour chaque tissu/cellule tous gènes confondus
-
+# Fonction qui calcule le nombre total de comptages pour chaque gène dans tous les tissus/cellules
+# data --> dico {gène : liste des comptages pour chaque tissu/cellule}
+# Exemple : {"GENE1": [2,10,15], "GENE2...]}
+    
     return {gene: sum(counts) for gene, counts in data.items()}
 
+# return (dico) --> {gène : somme des comptages des tissus/cellules}
+# Exemple : {"GENE1" : [27], "GENE2"...]}
 
 def total_count_tissue(data, tissues):
 
+# Fonction qui calcule le nombre de comptages pour chaque tissu/cellule pour tous les gènes
+# data (dico) --> {gène : comptages pour chaque tissu/cellule}
+# tissues (liste) --> noms des tissus/cellules dans le même ordre que les comptages dans data
+# Exemple : ["tissue-trachea", "tissue-lymphoblastic"...]
+
+# Nouveau dico avec tous les tissus/cellules à 0
+
     totals = {t: 0 for t in tissues}
+
+# Parcours chaque gène et additionne les comptages par tissu/cellule
 
     for counts in data.values():
         for i, value in enumerate(counts):
@@ -22,9 +28,12 @@ def total_count_tissue(data, tissues):
 
     return totals
 
+# return (dico) --> {tissus/cellules : somme totale des comptages}
+# Exemple : {"tissues-trachea" :[150], "tissue-lymphoblastic"...}
+
 def min_max_items(d):
 
-### Fonction qui identifie les clefs associées aux valeurs minimale et maximale dans le dico
+# Fonction qui identifie les clefs associées aux valeurs minimale et maximale dans un dico
 
     min_val = min(d.values())  
     max_val = max(d.values())
@@ -33,3 +42,9 @@ def min_max_items(d):
     max_items = [k for k, v in d.items() if v == max_val]
 
     return min_items, min_val, max_items, max_val
+
+ # return (tuples) --> 
+ # min_items (liste): clés avec la valeur minimale
+ # min_val (float/int): valeur minimale
+ # max_items (liste): clés avec la valeur maximale
+ # max_val (float/int): valeur maximale 
