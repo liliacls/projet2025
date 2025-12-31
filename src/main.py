e9d8d24a36797e630b67288e26f58363e38af6a7
liliacls <chals.lilia@gmail.com>
2025-12-31 14:53:25 +0100

    creation of HTML report

--- src/main.py
+++ src/main.py
@@ -1,8 +1,75 @@
-import matplotlib.pyplot as plt
+import os
+import webbrowser
 
 from io_utils import load_archs4
 from stats_utils import total_count_tissue, total_count_gene, summarize
 from plotting import plot_tissue_total, plot_gene_total
+
+def generate_html_report(path, tissues, data):
+    html = f"""<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Expression Matrix Report</title>
+    <style>
+    body {{
+        font-family: Arial, sans-serif;
+        margin: 40px;
+    }}
+
+    h1 {{
+        margin-top: 0;
+    }}
+
+    .summary-box {{
+        border: 2px solid black;
+        background-color: #fde7ef; /* rose pâle */
+        padding: 20px 25px;
+        border-radius: 8px;
+        max-width: 900px;
+        margin-bottom: 40px;
+    }}
+
+    .figure-box {{
+    border: 2px solid black;
+    padding: 15px;
+    border-radius: 8px;
+    margin-bottom: 40px;
+    max-width: 950px;
+}}
+
+    img {{
+        max-width: 900px;
+        margin-bottom: 40px;
+    }}
+</style>
+</head>
+<body>
+
+<div class="summary-box">
+    <h1>Expression Matrix — Summary Report</h1>
+
+    <p><strong>File:</strong> {path}</p>
+    <p><strong>Number of tissues / cells:</strong> {len(tissues)}</p>
+    <p><strong>Number of genes:</strong> {len(data)}</p>
+</div>
+
+<h2>Top 10 tissues / cells by total read counts</h2>
+<div class="figure-box">
+    <img src="top_tissues.png">
+</div>
+
+<h2>Top 10 genes by total read counts</h2>
+<div class="figure-box">
+    <img src="top_genes.png">
+</div>
+
+</body>
+</html>
+"""
+    with open("web_report/report.html", "w", encoding="utf-8") as f:
+        f.write(html)
+
 
 def main():
     path = "../data/ARCHS4.tsv.gz"
@@ -49,9 +116,16 @@ def main():
 
      
     # PARTIE 3 
+    os.makedirs("web_report", exist_ok=True)
 
     plot_tissue_total(tissue_totals, top_n=10, log_scale=True, output_path="web_report/top_tissues.png" )
+   
     plot_gene_total(gene_totals, top_n=10, log_scale=True, output_path="web_report/top_genes.png")
+
+    generate_html_report(path, tissues, data)
+
+    report_path = os.path.abspath("web_report/report.html")
+    webbrowser.open("file://" + report_path)
 
 if __name__ == "__main__":
     main()
--- src/plotting.py
+++ src/plotting.py
@@ -1,14 +1,8 @@
 import matplotlib.pyplot as plt
 
-from io_utils import load_archs4
-from stats_utils import total_count_tissue, total_count_gene
+#### PART 1 : TOP 10 des tissus/cellules avec le plus de comptes ###
 
-
-
-# PART 1 : TOP 10 des tissus/cellules avec le plus de comptes
-
-
-def plot_tissue_total(tissue_totals, top_n=10, log_scale=True):
+def plot_tissue_total(tissue_totals, top_n=10, log_scale=True, output_path=None):
     # Tri décroissant
     items = sorted(tissue_totals.items(), key=lambda x: x[1], reverse=True)
     if top_n:
@@ -24,33 +18,44 @@ def plot_tissue_total(tissue_totals, top_n=10, log_scale=True):
 
     # Mise en forme du plot visuellement
     for bar, value in zip(bars, values):
+        label = f"{int(value):,}".replace(",", " ")
         plt.text(
             bar.get_x() + bar.get_width() / 2,
             bar.get_height(),
-            f"{value:.2f}",
+            label,
             ha="center",
             va="bottom",
-            fontsize=9
+            fontsize=9,
+            fontweight="bold"
         )
 
-    plt.xlabel("Tissues / Cells")
-    plt.ylabel("Total read counts")
-    plt.title("Total read counts per tissue")
+    plt.xlabel("Tissues / Cells", fontsize=12, fontweight="bold")
+    plt.ylabel("Total read counts", fontsize=12, fontweight="bold")
+    plt.title("Total read counts per tissue", fontsize=14, fontweight="bold", pad=15)
 
     if log_scale:
         plt.yscale("log")
-        plt.ylabel("Total read counts (log scale)")
+        plt.ylabel("Total read counts (log scale)", fontsize=12, fontweight="bold")
 
-    plt.xticks(rotation=45, ha="right")
+    plt.xticks(fontsize=10, rotation=45, ha="right")
+    plt.yticks(fontsize=10)
+
+    ax = plt.gca()
+    for spine in ax.spines.values():
+        spine.set_linewidth(1.2)
+
     plt.tight_layout()
+
+    if output_path:
+        plt.savefig(output_path, dpi=300)
+        plt.close()
+    else:
+        plt.show()
     
 
+### PART 2 : TOP 10 des gènes avec le plus de comptes ###
 
-
-# PART 2 : TOP 10 des gènes avec le plus de comptes
-
-
-def plot_gene_total(gene_totals, top_n=10, log_scale=True):
+def plot_gene_total(gene_totals, top_n=10, log_scale=True, output_path=None):
     # Tri décroissant
     items = sorted(gene_totals.items(), key=lambda x: x[1], reverse=True)
     if top_n:
@@ -66,24 +71,36 @@ def plot_gene_total(gene_totals, top_n=10, log_scale=True):
 
     # Mise en forme du plot visuellement
     for bar, value in zip(bars, values):
+        label = f"{int(value):,}".replace(",", " ")
         plt.text(
             bar.get_x() + bar.get_width() / 2,
             bar.get_height(),
-            f"{value:.2f}",
+            label,
             ha="center",
             va="bottom",
-            fontsize=9
+            fontsize=9,
+            fontweight="bold"
         )
 
-    plt.xlabel("Genes")
-    plt.ylabel("Total read counts")
-    plt.title("Top genes by total read count")
+    plt.xlabel("Genes", fontsize=12, fontweight="bold")
+    plt.ylabel("Total read counts (log scale)", fontsize=12, fontweight="bold")
+    plt.title("Top genes by total read count", fontsize=14, fontweight="bold", pad=15)
 
     if log_scale:
         plt.yscale("log")
         plt.ylabel("Total read counts (log scale)")
 
-    plt.xticks(rotation=45, ha="right")
+    plt.xticks(rotation=45, ha="right", fontsize=10)
+    plt.yticks(fontsize=10)
+
+    ax = plt.gca()
+    for spine in ax.spines.values():
+        spine.set_linewidth(1.2)
+
     plt.tight_layout()
 
-
+    if output_path:
+        plt.savefig(output_path, dpi=300)
+        plt.close()
+    else:
+        plt.show()
