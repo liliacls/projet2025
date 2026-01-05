5c4b5c2c7d4a111c9d325fccc21bbc2b6758bbf6
liliacls <chals.lilia@gmail.com>
2026-01-05 18:40:38 +0100

    update comments

--- tests/test_stats_utils.py
+++ tests/test_stats_utils.py
@@ -1,7 +1,7 @@
 """
-Unit tests for statistical computation functions on a count matrix
+Unit tests for statistical computation functions on a count matrix.
 
-An artificial dataset is used
+An artificial dataset is used.
 
 Functions tested 
 ----------------
@@ -33,7 +33,7 @@ def test_total_count_gene():
     
     result = total_count_gene(data)
     
-     # Verification of total counts computed for each gene
+    # Verification of total counts computed for each gene
     assert result == { 'GENE1': pytest.approx(187.4),
                        'GENE2': pytest.approx(777.4),
                        'GENE3': pytest.approx(396.8),
@@ -51,11 +51,11 @@ def test_total_count_tissue():
     
     result = total_count_tissue(tissues, data)
     
-     # Expected total counts per tissue: 
-     # tissue1 : 150.3 + 734.9 + 333.0 + 180.0 = 1398.2
-     # tissue2 : 25.0 + 36.0 + 54.2 + 0.0 = 115.2
-     # tissue3 : 7.1 + 2.0 + 5.0 + 7.4 = 21.5
-     # tissue4 : 5.0 + 4.5 + 4.6 + 0.0 = 14.1
+    # Expected total counts per tissue: 
+    # tissue1 : 150.3 + 734.9 + 333.0 + 180.0 = 1398.2
+    # tissue2 : 25.0 + 36.0 + 54.2 + 0.0 = 115.2
+    # tissue3 : 7.1 + 2.0 + 5.0 + 7.4 = 21.5
+    # tissue4 : 5.0 + 4.5 + 4.6 + 0.0 = 14.1
     
     assert result == {
         "tissue1": pytest.approx(1398.2),
